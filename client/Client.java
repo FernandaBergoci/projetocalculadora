8b12a2ca2b157f274332240cbbd2854891c071d0
FernandaBergoci <fernandabergoci@gmail.com>
2023-04-22 12:00:46 -0300

    conexão cliente servidor

--- client/Client.java
+++ client/Client.java
@@ -1,9 +1,31 @@
 package client;
 
+import java.net.Socket;
 
 public class Client {
 
     public static void main(String[] args) {
-        
+        int porta = 54321;
+        String ip = "127.0.0.1"; // localhost
+        Socket socket;
+
+        try {
+            socket = new Socket(ip,porta);
+            System.out.println("Conectado com o servidor.");
+         } catch (Exception e) {
+             System.out.println("Erro: " + e.getMessage());
+             return;
+         }
+ 
+        // while(){
+ 
+         //}
+ 
+         try {
+             System.out.println("Encerrando o cliente");
+             socket.close();
+         } catch (Exception e) {
+             
+         }
     }
 }
--- server/Server.java
+++ server/Server.java
@@ -1,7 +1,40 @@
 package server;
 
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
     public static void main(String[] args) {
-        
+        ServerSocket serverSocket; //protocolo TCP/IP ele que se preocupa em como entragar a mensagem
+        int porta = 54321;
+        Socket socketClient;
+
+        //processo de bind solicita porta
+        try {
+           serverSocket = new ServerSocket(porta); 
+           System.out.println("Servidor disponível na porta: " + porta);
+        } catch (Exception e) {
+            System.out.println("Erro: " + e.getMessage());
+            return;
+        }
+
+        //while(){
+            try {
+                System.out.println("Aguardando o cliente...");
+                //programa para e aguarda a mensagem
+                socketClient = serverSocket.accept();
+                System.out.println("Conectado com " + socketClient.getInetAddress().getHostAddress());
+            } catch (Exception e) {
+                // TODO: handle exception
+            }
+        //}
+
+        try {
+            System.out.println("Encerrando o servidor");
+            serverSocket.close();
+        } catch (Exception e) {
+            
+        }
+
     }
 }
